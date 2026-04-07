140e3549e77e01d164b7af4b1a3dc5d6b4a2f838
Camilo Castellanos <camilo.castellanos.puentes@gmail.com>
2026-04-07 17:55:51 -0500

    Creación de componente profile

---
+++ src/app/shared/components/profile/profile.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { bootstrapFiletypeDoc, bootstrapGithub, bootstrapLinkedin } from '@ng-icons/bootstrap-icons';
+import { ionMail } from '@ng-icons/ionicons';
+import { heroDocumentArrowDownSolid } from '@ng-icons/heroicons/solid';
+
+
+@Component({
+  selector: 'profile',
+  imports: [NgIcon],
+  templateUrl: './profile.html',
+  styleUrl: './profile.css',
+  viewProviders: [provideIcons({
+    bootstrapLinkedin, bootstrapFiletypeDoc, bootstrapGithub, ionMail, heroDocumentArrowDownSolid
+  })]
+})
+export class Profile {
+
+  sizeIcon: string = "25";
+  colorIcon: string = "var(--accent)";
+  urlGit: string = 'https://github.com/CamiloCastellanos';
+  urlLinkedin: string = 'https://www.linkedin.com/in/juan-camilo-castellanos-puentes-0b6376152/';
+  email: string = "camilo.castellanos.puentes@gmail.com";
+  urlCV: string = "https://drive.google.com/file/d/1iiOjGLteQNDpTcoNLteAJIJM5gcYWjq7/view?usp=drive_link";
+  curriculumVitaeEn = "https://drive.google.com/file/d/1uA8rPnOOOQmZM9FJQgI1m5DXGEt_MpN5/view?usp=drive_link";
+  curriculumVitaeEs = "https://drive.google.com/file/d/1iiOjGLteQNDpTcoNLteAJIJM5gcYWjq7/view?usp=drive_link";
+
+  copyMail() {
+
+  }
+}
