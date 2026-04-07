b9666ad1858deadee8368ca2f03a50cbd559f518
Camilo Castellanos <camilo.castellanos.puentes@gmail.com>
2026-04-07 17:51:57 -0500

    Se realizan las implementaciones necesarias en el app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,34 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideTransloco } from '@ngneat/transloco';
+
+const firebase = {
+  apiKey: "AIzaSyCPo7JTRBbDoU2yIbhbTNREWACO7xlkMew",
+  authDomain: "portafolioangular-4f64f.firebaseapp.com",
+  databaseURL: "https://portafolioangular-4f64f.firebaseio.com",
+  projectId: "portafolioangular-4f64f",
+  storageBucket: "portafolioangular-4f64f.appspot.com",
+  messagingSenderId: "465578885884",
+  appId: "1:465578885884:web:ce4edca29843abe4940d79",
+  measurementId: "G-7C4MYXWBW9"
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebase)),
+    provideFirestore(() => getFirestore()),
+    provideTransloco({
+      config: {
+        availableLangs: ['es', 'en'],
+        defaultLang: 'es',
+        reRenderOnLangChange: true,
+        prodMode: false
+      }
+    })
   ]
 };
