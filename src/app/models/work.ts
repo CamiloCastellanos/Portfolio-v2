f358b675f9619c0871f85003ec826a152bfe10dc
Camilo Castellanos <camilo.castellanos.puentes@gmail.com>
2026-04-07 17:58:38 -0500

    Creación de modelo Work

---
+++ src/app/models/work.ts
@@ -0,0 +1,13 @@
+export class Work {
+  title: string = '';
+  description: string = '';
+  descriptionEn: string = '';
+  image: string = '';
+  url: string = '#';
+  startDate: string = '';
+  startDateEn: string = '';
+  endDate: string = 'Actual';
+  endDateEn: string = 'Present';
+  order: number = 0
+  icon: string = ""
+}
