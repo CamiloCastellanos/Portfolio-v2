d653e64521a7e93089745371c25e7e1688667bd3
Camilo Castellanos <camilo.castellanos.puentes@gmail.com>
2026-04-07 22:00:05 -0500

    Adición de traducción a la pagina about

--- src/app/pages/about-me/about-me.ts
+++ src/app/pages/about-me/about-me.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+//Transloco
+import { TranslocoPipe } from '@ngneat/transloco';
 
 @Component({
   selector: 'about-me',
-  imports: [],
+  imports: [TranslocoPipe],
   templateUrl: './about-me.html',
   styleUrl: './about-me.css',
 })
-export class AboutMe {}
+export class AboutMe { }
