aea7079f90daa80161c426783220a9dda527d8fe
Camilo Castellanos <camilo.castellanos.puentes@gmail.com>
2026-04-07 22:25:47 -0500

    Se ajusta el card inicial en el responsive, ademas se ajustan textos en el traductor

--- src/app/pages/about-me/about-me.ts
+++ src/app/pages/about-me/about-me.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 //Transloco
 import { TranslocoPipe } from '@ngneat/transloco';
+//
+import { Profile } from '../../shared/components/profile/profile';
 
 @Component({
   selector: 'about-me',
-  imports: [TranslocoPipe],
+  imports: [TranslocoPipe, Profile],
   templateUrl: './about-me.html',
   styleUrl: './about-me.css',
 })
