ec44fbf77fafb9198d94ab0b304f6f2e3f46c910
Camilo Castellanos <camilo.castellanos.puentes@gmail.com>
2026-04-07 22:59:59 -0500

    Se ajustan estilos de about me , resume , layout y navbar

--- src/app/pages/resume/resume.ts
+++ src/app/pages/resume/resume.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { collectionData, Firestore, collection } from '@angular/fire/firestore'; // ✅ todo desde aquí
+// Transloco
+import { TranslocoPipe } from '@ngneat/transloco';
+//
 import { Work } from '../../models/work';
 import { TimeLine } from './components/time-line/time-line';
+import { ToolsLanguages } from '../../shared/components/tools-languages/tools-languages';
 
 @Component({
   selector: 'resume',
-  imports: [TimeLine],
+  imports: [TimeLine, TranslocoPipe, ToolsLanguages],
   templateUrl: './resume.html',
   styleUrl: './resume.css',
 })
