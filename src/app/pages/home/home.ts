dd2d2013677417a916e532faaf9bb59fadfd01d3
Camilo Castellanos <camilo.castellanos.puentes@gmail.com>
2026-04-09 17:01:41 -0500

    se actualiza la visualización del CV desde el home (para mostrarlo en EN o SP)

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 //
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { bootstrapDownload, bootstrapFolder } from '@ng-icons/bootstrap-icons';
 // Transloco
-import { TranslocoPipe } from '@ngneat/transloco';
+import { TranslocoPipe, TranslocoService } from '@ngneat/transloco';
 //
 import { CallToScroll } from './components/call-to-scroll/call-to-scroll';
 import { ScrollService } from '../../shared/services/scroll-service';
@@ -17,13 +17,19 @@ import { ScrollService } from '../../shared/services/scroll-service';
     bootstrapDownload, bootstrapFolder
   })]
 })
-export class Home {
+export class Home implements OnInit {
   sizeIcon: string = "15";
   urlCV: string = "https://drive.google.com/file/d/1iiOjGLteQNDpTcoNLteAJIJM5gcYWjq7/view?usp=drive_link";
-  curriculumVitaeEn = "https://drive.google.com/file/d/1uA8rPnOOOQmZM9FJQgI1m5DXGEt_MpN5/view?usp=drive_link";
-  curriculumVitaeEs = "https://drive.google.com/file/d/1iiOjGLteQNDpTcoNLteAJIJM5gcYWjq7/view?usp=drive_link";
+  private readonly urlCVEn = "https://drive.google.com/file/d/1uA8rPnOOOQmZM9FJQgI1m5DXGEt_MpN5/view?usp=drive_link";
+  private readonly urlCVEs = "https://drive.google.com/file/d/1iiOjGLteQNDpTcoNLteAJIJM5gcYWjq7/view?usp=drive_link";
 
-  constructor(private readonly scrollService: ScrollService) { }
+  constructor(private readonly transloco: TranslocoService, private readonly scrollService: ScrollService) { }
+
+  ngOnInit() {
+    this.transloco.langChanges$.subscribe((lang) => {
+      this.urlCV = lang == 'es' ? this.urlCVEs : this.urlCVEn;
+    });
+  }
 
   scrollToProjects() {
     this.scrollService.scrollTo('projects');
